bfff6d288bc2af8915c5508831c2ff17f8161627
xXGrowGuruXx <116651235+xXGrowGuruXx@users.noreply.github.com>
2025-02-24 16:47:53 +0100

    Update Bestseller - Line

--- bestseller.js
+++ bestseller.js
@@ -64,7 +64,7 @@ function createProductCard(product) {
     details.forEach((detail, index) => {
         if (index > 0) {
             const itemLine = document.createElement("hr");
-            itemLine.style.border = "1px solid #ccc";
+            itemLine.style.border = "3px solid black";
             productInfo.appendChild(itemLine);
         }
 
