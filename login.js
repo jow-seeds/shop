4d3f1fc7517aa290c0ab83c409d90297e4e0b3a9
xXGrowGuruXx <116651235+xXGrowGuruXx@users.noreply.github.com>
2025-02-25 14:58:21 +0100

    Update Login - CloseEvent 2

--- login.js
+++ login.js
@@ -4,8 +4,8 @@ const profileModal = document.getElementById("profileModal");
 
 // Schließen des Modals bei Klick außerhalb
 document.addEventListener("click", (event) => {
-    if (!loginModal.contains(event.target) && event.target !== profileButton && event.target !== profileModal) {
-      loginModal.classList.remove("show");
-      setTimeout(() => loginModal.classList.add("hidden"), 300);
+    if (!event.target.closest(loginModal)) {
+        loginModal.classList.remove("show");
+        setTimeout(() => loginModal.classList.add("hidden"), 300);
     }
 });
