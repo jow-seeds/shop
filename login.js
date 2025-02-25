759dd81e7be2c80643f7bce821ab500a33fdf187
xXGrowGuruXx <116651235+xXGrowGuruXx@users.noreply.github.com>
2025-02-25 12:13:02 +0100

    Update Profile - LoginModal 3

--- login.js
+++ login.js
@@ -1,1 +1,9 @@
-let loginModal = document.getElementById("loginOrRegister")
+let loginModal = document.getElementById("loginOrRegister");
+
+// Schließen des Modals bei Klick außerhalb
+document.addEventListener("click", (event) => {
+    if (!loginModal.contains(event.target)) {
+      loginModal.classList.remove("show");
+      setTimeout(() => loginModal.classList.add("hidden"), 300);
+    }
+  });
